d31aeea594ef2f4ffac2ee9d6bf6efbeb4b2e574
Samer Afach <afach@uni-mainz.de>
2016-12-01 20:43:50 +0100

    Modified simulators to work permanently with a fake sinusoid as data for all channels.

--- misc/write_dummy_cavities.py
+++ misc/write_dummy_cavities.py
@@ -4,33 +4,45 @@
 #
 #   US, PTB 2016
 
+##################
+# Script modified by Samer Afach (Uni-Mainz), 01.12.2016
+# The script now produces time coherent data that simulates atomic clocks experiment output
+# The output is simply a sinosoid with frequency f
+##################
 
 import time, os
 import glob
 import datetime as dt
+import numpy as np
+import math
 
 #Set the filename and open the file
 outpath = 'D:\\ClockData\\Cavities\\'
 sample = '160324 000000.002  30089915.15010000020   8587663.48739999905         0.0000000\n'
-
-
+num_columns = 3
+with_flags = False
 fout_name = format(dt.datetime.now(),'%y%m%d') + '_1_Frequ.txt'
 
-fout=open(outpath+fout_name,'w')
-i=0
-i1=0
-sr=1000
-write_now = dt.datetime.now()
-while i < sr*60*60*1:  # just one hour
+fout = open(outpath+fout_name,'w')
+i = 0
+sr = 1000
+start_time = dt.datetime.now()
+write_now = start_time
+f = 0.07  # frequency
+while True:  # just one hour
     real_now = dt.datetime.now()
+    t = (write_now - start_time).total_seconds()
+    data = np.array(['{:.20f}'.format(np.float64(i+1)*np.sin(2*math.pi*f*t)) for i in range(num_columns)])
+    data_str = str(data).replace("[", "").replace("]", "").replace("'", "").replace("\n", "") + "\n"
+
     if (write_now - real_now).total_seconds() > 0:
         time.sleep((write_now - real_now).total_seconds())
     if i % sr == 0:
         write_now += dt.timedelta(seconds=1 / sr)
-        s = format(write_now, '%y%m%d*%H%M%S.%f')[0:17] + sample[17:]
+        s = format(write_now, '%y%m%d*%H%M%S.%f')[0:17] + (" FFFFFFFF" if with_flags else "") + " " + data_str
     else:
         write_now += dt.timedelta(seconds=1 / sr)
-        s = format(write_now, '%y%m%d %H%M%S.%f')[0:17] + sample[17:]
+        s = format(write_now, '%y%m%d %H%M%S.%f')[0:17] + (" FFFFFFFF" if with_flags else "") + " " + data_str
     fout.write(s)
     fout.flush()
     i += 1
--- misc/write_dummy_comb.py
+++ misc/write_dummy_comb.py
@@ -5,32 +5,45 @@
 #   US, PTB 2016
 #   US, PTB 2016
 
+##################
+# Script modified by Samer Afach (Uni-Mainz), 01.12.2016
+# The script now produces time coherent data that simulates atomic clocks experiment output
+# The output is simply a sinosoid with frequency f
+##################
 
 import time, os
 import glob
 import datetime as dt
+import numpy as np
+import math
 
 #Set the filename and open the file
 outpath = 'D:\\ClockData\\comb\\'
 sample = '161101*000000.299 FFFFFFFF  32365919.99990969900  19999999.98439349980  23359875.19833400100  23572738.83385109900  51320112.94533299650  54701611.62395049630  62468051.41144129630  34201269.49379400160  34999999.99953600020  20000000.00156589970  10000000.00132700060  59725627.47657729680  19999999.98443600160  23359875.19851360100  23572738.83431870120  51320112.96735619750  61425204.12941499800  62468049.96998640150  46581938.69217439740  46581938.69214440140         1.4414549\n'
-
+num_columns = 21
+with_flags = True
 fout_name = format(dt.datetime.now(),'%y%m%d') + '_1_Frequ.txt'
 
-fout=open(outpath+fout_name,'w')
-i=0
-i1=0
-sr=1
-write_now = dt.datetime.now()
-while i < sr*60*60*1:  # just one hour
+fout = open(outpath+fout_name,'w')
+i = 0
+sr = 1
+start_time = dt.datetime.now()
+write_now = start_time
+f = 0.07
+while True:  # just one hour
     real_now = dt.datetime.now()
+    t = (write_now - start_time).total_seconds()
+    data = np.array(['{:.20f}'.format(np.float64(i+1)*np.sin(2*math.pi*f*t)) for i in range(num_columns)])
+    data_str = str(data).replace("[", "").replace("]", "").replace("'", "").replace("\n", "") + "\n"
+
     if (write_now - real_now).total_seconds() > 0:
         time.sleep((write_now - real_now).total_seconds())
     if i % sr == 0:
         write_now += dt.timedelta(seconds=1 / sr)
-        s = format(write_now, '%y%m%d*%H%M%S.%f')[0:17] + sample[17:]
+        s = format(write_now, '%y%m%d*%H%M%S.%f')[0:17] + (" FFFFFFFF" if with_flags else "") + " " + data_str
     else:
         write_now += dt.timedelta(seconds=1 / sr)
-        s = format(write_now, '%y%m%d %H%M%S.%f')[0:17] + sample[17:]
+        s = format(write_now, '%y%m%d %H%M%S.%f')[0:17] + (" FFFFFFFF" if with_flags else "") + " " + data_str
     fout.write(s)
     fout.flush()
     i += 1
